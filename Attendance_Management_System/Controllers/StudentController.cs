5ea5624fc1403c9feaf9d4d907c28ca8a5cb5cf2
jamesmaano <jamesneil.maano@dbtc-cebu.edu.ph>
2026-03-07 20:32:55 +0800

    Added Backend files

---
+++ Attendance_Management_System/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Attendance_Management_System.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
