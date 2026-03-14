f5014851d213d595371d11416e58d8d884ad42d6
jamesmaano <jamesneil.maano@dbtc-cebu.edu.ph>
2026-03-14 08:55:38 +0800

    Update project files

---
+++ Backend/Attendance_Management_System/Interfacess/Iauthservice.cs
@@ -0,0 +1,10 @@
+﻿using Attendance_Management_System.Models;
+
+namespace Attendance_Management_System.Interfacess
+{
+    public interface IAuthService
+    {
+        Task<LoginResponse?> LoginAsync(LoginRequest request);
+        Task SeedAdminAsync();
+    }
+}
