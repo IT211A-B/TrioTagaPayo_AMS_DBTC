69b16f6215e9391e0465f2ff9a73def9a9d47690
HeavenlyMoreno <rimarkrafael.moreno@dbtc-cebu.edu.ph>
2026-05-15 21:18:59 +0800

    Delete Backend/Repositories directory

--- Backend/Repositories/Interfaces/IAttendanceFilterRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Attendance_Management_System.Models;
-
-namespace Attendance_Management_System.Repositories.Interfaces
-{
-    public interface IAttendanceFilterRepository
-    {
-        Task<IEnumerable<Attendance>> GetByFilterAsync(int courseId, DateOnly from, DateOnly to);
-    }
-}
