4366b44722d219c9fdec5052ec266bb13d6b8cc7
HeavenlyMoreno <rimarkrafael.moreno@dbtc-cebu.edu.ph>
2026-05-15 21:22:54 +0800

    Delete Backend/Attendance_Management_System/Attendance_Management_System directory

--- Backend/Attendance_Management_System/Attendance_Management_System/Interfacess/IJwtTokenGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Attendance_Management_System.Models;
-
-namespace Attendance_Management_System.Interfacess
-{
-    public interface IJwtTokenGenerator
-    {
-        string Generate(User user);
-    }
-}
--- Backend/Attendance_Management_System/Attendance_Management_System/Repositories/Interfaces/IAttendanceBulkRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Attendance_Management_System.Models;
-
-namespace Attendance_Management_System.Repositories.Interfaces
-{
-    public interface IAttendanceBulkRepository
-    {
-        Task<IEnumerable<Attendance>> GetByIdsWithDetailsAsync(List<int> ids);
-    }
-}
